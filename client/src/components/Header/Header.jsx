b2911754ff41c16f4c06d4938c0a1bbbfffcc108
Some-O-ne <sasha.go.go.go.go@gmail.com>
2024-04-30 16:58:37 +0300

    added

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,19 +1,13 @@
 import styles from './Header.module.css'
-import { Link } from "react-router-dom";
+import Nav from "../Header/Nav/Nav";
 export default function (){
     return (
         <div className={styles.header}>
             <div className={styles.brand}>
-                <img src="./logo.png" alt="logo" width={50} height={50} />
+                <img src="./logo.png" alt="logo" />
                 <h1>Торпеда</h1>
             </div>
-            <div className={styles.links}>
-                <Link to="/">Главная</Link>
-                <Link to="/login">Вход</Link>
-                <Link to="/register">Регистрация</Link>
-                <Link to="/about">О проекте</Link>
-                <Link to="/contacts">Контакты</Link>
-            </div>
+            <Nav/>
         </div>
     )
 }
