05c01d0f2240270b50c36db9008d09a751ba0af5
Some-O-ne <sasha.go.go.go.go@gmail.com>
2024-05-15 20:36:39 +0300

    фыв

--- client/src/pages/Chat/ChatStack.jsx
+++ client/src/pages/Chat/ChatStack.jsx
@@ -5,7 +5,7 @@ import { getChat } from '../../utils/queries/chats.js';
 export default function(props) {
     function renderChats() {
         return props.chats ? props.chats.map(chat => (
-            <div key={chat.id} onClick={() => {getChat(chat.id).then((response) => props.setCurrentChat(response))}}>{chat.name}</div>
+            <div key={chat.id} className={style.chat} onClick={() => {getChat(chat.id).then((response) => props.setCurrentChat(response))}}>{chat.name}</div>
         )) : <p>Нет чатов</p>
         }
     return (
