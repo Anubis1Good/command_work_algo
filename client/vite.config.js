2f70398b7a90f737412826c6fcb0dccf04267b7b
Anubis1Good <vlad.polyakov.96@inbox.ru>
2024-05-24 13:45:28 +0300

    repaire vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host:"192.168.0.104",
+    // host:"192.168.0.104",
+    host:"127.0.0.1",
     proxy: {
       '/api/v1': 'http://localhost:3000'
     }
